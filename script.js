06a77e90e6873e0fb3b0d004a6cf44ca0bb5818e
Deepak8717 <sendit2deepak@gmail.com>
2021-02-21 20:05:12 +0530

    added the the todo onclick functionality

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const todoList = document.getElementById("todo-list");
 const todoInput = document.getElementById("txtInput");
 const todoSubmit = document.getElementById("todoSubmit");
-
+const clear = document.querySelector(".clear");
 const todoArry = [];
+
 function clearTodos() {
   while (todoList.firstChild) {
     todoList.removeChild(todoList.lastChild);
   }
 }
+
 function renderTodos() {
   clearTodos();
   todoArry.forEach((todo) => {
     const li = document.createElement("li");
     li.innerHTML =
-      "<div>" + todo.task + "</div><div><button>Delete</button></div>";
+      "<div>" +
+      todo.task +
+      "</div><div><button class='delete'>Delete</button></div>";
     todoList.appendChild(li);
   });
 }
+
 todoSubmit.addEventListener("click", () => {
   const d = new Date();
   const obj = { id: d.getTime(), task: todoInput.value };
@@ -26,3 +31,4 @@ todoSubmit.addEventListener("click", () => {
   renderTodos();
   todoInput.value = "";
 });
+clear.addEventListener("click", clearTodos);
